6ad6516c2a9c846edbfe015dff77227def52ada3
Swapnil Iqbal <swapniliqbal@gmail.com>
2018-08-13 04:29:46 -0700

    Backend form submission using Firebase

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,58 @@
-// Code for smooth scrolling from landing section down arrow to about section
+//Initialize Firebase, has to be at the top
+var config = {
+    apiKey: "AIzaSyBlJPK4UnZBVtbfAqmkExfgB_8L4VHF0mY",
+    authDomain: "swapnil-portfolio-website.firebaseapp.com",
+    databaseURL: "https://swapnil-portfolio-website.firebaseio.com",
+    projectId: "swapnil-portfolio-website",
+    storageBucket: "swapnil-portfolio-website.appspot.com",
+    messagingSenderId: "1070607632934"
+};
+firebase.initializeApp(config);
+
+// Implementing firebase backend 
+
+//Reference messages collection:
+var messagesRef = firebase.database().ref('messages');
+
+//Save messages to firebase:
+//MessagesRef is like a table. All I am doing is pushing the data to the table
+function saveMessage(name, email, message) {
+    var newMessagesRef = messagesRef.push();
+    newMessagesRef.set({
+        name: name,
+        email: email,
+        message: message
+    });
+};
+
+var validated = false;
+
+// Runs submission to firebase function:
+    document.getElementById('submit').addEventListener('click', submitForm);
+
+//Submit the form:
+function submitForm(e) {
+    e.preventDefault();
+
+    //Get all the data from the form:
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    //Saving the message
+    saveMessage(name, email, message);
+
+    //Clear the fields after submitting:
+    document.getElementById('contactForm').reset();
+
+    //Display success message, then erase after 10 seconds:
+    document.querySelector(".message-sent").style.display = "block";
+    setTimeout(function () {
+        document.querySelector(".message-sent").style.display = "none";
+    }, 3000);
+};
+
+// Smooth scroll code made using JQuery:
 $(document).ready(function () {
 
     var scrollLink = $('.scroll');
@@ -25,6 +79,6 @@ $(document).ready(function () {
             }
         })
 
-    })
-
+    });
+    // End of smooth scroll
 });
