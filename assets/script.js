da010b618e9d1446727e1bb73a01b21000e68a1b
Swapnil Iqbal <36313876+swapnil233@users.noreply.github.com>
2018-08-13 07:03:44 -0400

    smooth scroll bug fix v1

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,3 @@
-// Firebase has been initialized in firebaseInit.js
-
-
-// Implementing firebase backend:
-
-//Reference messages collection:
-var messagesRef = firebase.database().ref('messages');
-
-//Save messages to firebase:
-//MessagesRef is like a table. All I am doing is pushing the data to the table
-function saveMessage(name, email, message) {
-    var newMessagesRef = messagesRef.push();
-    newMessagesRef.set({
-        name: name,
-        email: email,
-        message: message
-    });
-};
-
-var validated = false;
-
-// Runs submission to firebase function:
-    document.getElementById('submit').addEventListener('click', submitForm);
-
-//Submit the form:
-function submitForm(e) {
-    e.preventDefault();
-
-    //Get all the data from the form:
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-
-    //Saving the message
-    saveMessage(name, email, message);
-
-    //Clear the fields after submitting:
-    document.getElementById('contactForm').reset();
-
-    //Display success message, then erase after 10 seconds:
-    document.querySelector(".message-sent").style.display = "block";
-    setTimeout(function () {
-        document.querySelector(".message-sent").style.display = "none";
-    }, 3000);
-};
-
 // Smooth scroll code made using JQuery:
 $(document).ready(function () {
 
@@ -71,6 +25,51 @@ $(document).ready(function () {
             }
         })
 
-    });
-    // End of smooth scroll
+    }); // End of smooth scroll
 });
+
+// Firebase has been initialized in firebaseInit.js
+
+
+// Implementing firebase backend:
+
+//Reference messages collection:
+var messagesRef = firebase.database().ref('messages');
+
+//Save messages to firebase:
+//MessagesRef is like a table. All I am doing is pushing the data to the table
+function saveMessage(name, email, message) {
+    var newMessagesRef = messagesRef.push();
+    newMessagesRef.set({
+        name: name,
+        email: email,
+        message: message
+    });
+};
+
+var validated = false;
+
+// Runs submission to firebase function:
+document.getElementById('submit').addEventListener('click', submitForm);
+
+//Submit the form:
+function submitForm(e) {
+    e.preventDefault();
+
+    //Get all the data from the form:
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    //Saving the message
+    saveMessage(name, email, message);
+
+    //Clear the fields after submitting:
+    document.getElementById('contactForm').reset();
+
+    //Display success message, then erase after 10 seconds:
+    document.querySelector(".message-sent").style.display = "block";
+    setTimeout(function () {
+        document.querySelector(".message-sent").style.display = "none";
+    }, 3000);
+};
